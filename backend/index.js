5283dcb95cfb7e18c5ec9c2df4043ad65c883ba4
Massoud Bakhtyari <Massoud.bakhtyari@yahoo.com>
2021-11-17 23:58:53 -0800

    database connected

--- backend/index.js
+++ backend/index.js
@@ -4,21 +4,44 @@ const app = express()
 
 const mysql = require('mysql')
 
-const db = mysql.createPool({
+var db = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'password',
-    database:'Restaurent-Database',
+    password:'Energy-Team2021',
+    database:'dbrestaurant',
+    port:"3306"
 
+});
+
+db.connect((err) =>{
+    if(err) {
+        throw err
+    } else {
+        console.log("connected")
+    }
 })
-
-app.get('/', (req, res) => {
-    const sqlInsert = "INSERT INTO table1(name,password) VALUES {'Massoud', 'Thanks'};"
-    db.query(sqlInsert, (err, result)=> {
-        res.send("hello world"); 
-    })
+// Create Table 
+/*db.query('CREATE TABLE account(userID varchar(60)  PRIMARY KEY, password VARCHAR(255) NOT NULL)',(err,rows) => {
+    if(err) {
+        throw err
+    } else {
+        console.log(rows)
+    }
     
-}); 
+})
+*/
+// Insert into table that we have created 
+db.query("INSERT INTO account(userID,password) VALUES('Massoud','kdajdkf')", (err, rows) => {
+    if(err) {
+        throw err
+    } else {
+        console.log("Data SENT BOIS"); 
+        console.log(rows)
+
+    } 
+})      
+
 app.listen(3001, () => {
     console.log("runnign on port 3001");
 });
+

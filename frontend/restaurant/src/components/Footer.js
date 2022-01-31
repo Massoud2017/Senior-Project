ea3a43ee9ebf734e9ca3ae02d3e3fb2e3458da86
Zoha Alvi <zalvi@csus.edu>
2022-01-30 19:15:51 -0800

    Misba/Zoha: Updated About page to include template for about paragraph, programmed text and image to auto-size with screen size, and updated footer to embed social media links and icons.

--- frontend/restaurant/src/components/Footer.js
+++ frontend/restaurant/src/components/Footer.js
@@ -2,13 +2,19 @@ import React from "react";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import "./components-styles/Footer.css";
+import Logo_y from './yelp_logo.png';
+import Logo_f from './f_logo.png';
+import Logo_i from './Insta_logo.png';
+
 
 function Footer() {
   return (
     <div className="footer">
       <p>6116 Mack Road Sacramento, CA 95823</p>
       <div className="socialMedia">
-        <InstagramIcon /> <FacebookIcon />
+        <a target="_blank" href="https://www.instagram.com/phoru.usa/?hl=en"><img src={Logo_i} height={35} width={35} /></a> {"                           "} 
+        <a target="_blank" href="https://www.facebook.com/phorurestaurant/"><img src={Logo_f} height={35} width={35} /></a>  {"                           "}  
+        <a target="_blank" href="https://www.yelp.com/biz/pho-ru-vietnamese-restaurant-sacramento"><img src={Logo_y} height={35} width={80}/></a>
       </div>
       <p>&copy; 2022</p>
     </div>
--- frontend/restaurant/src/screens/About.js
+++ frontend/restaurant/src/screens/About.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import about from './about_us.jpeg'
+import image from './components/style.css'
+
 function About() {
     return (
-        <div className="about">
-        <img src={about}/>
+        <div style = {{paddingLeft: '2em', paddingRight: '2em'}}>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio pellentesque diam volutpat commodo sed egestas. Tincidunt augue interdum velit euismod in pellentesque. Cras adipiscing enim eu turpis egestas pretium aenean pharetra. Morbi tincidunt augue interdum velit euismod. Interdum velit laoreet id donec ultrices tincidunt arcu non. Eu scelerisque felis imperdiet proin fermentum leo vel orci. Et netus et malesuada fames ac turpis. Ultrices vitae auctor eu augue ut lectus. Donec et odio pellentesque diam. Porttitor eget dolor morbi non arcu. Ut consequat semper viverra nam libero justo laoreet sit amet. Aenean vel elit scelerisque mauris"</p>
+            <div className='image'>
+                <img src={about}/> 
+            </div> 
         </div>
+
     )
 }
-
 export default About;

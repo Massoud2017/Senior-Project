b9eb000815bc94f5be93c3dc0e73722dd8290191
Massoud Bakhtyari <Massoud.bakhtyari@yahoo.com>
2022-01-31 21:46:04 -0800

    organized files and get rid of 5 high vulnerabilities

--- frontend/restaurant/src/components/Footer.js
+++ frontend/restaurant/src/components/Footer.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./components-styles/Footer.css";
-import Logo_y from './yelp_logo.png';
-import Logo_f from './f_logo.png';
-import Logo_i from './Insta_logo.png';
+import Logo_y from '../assets/yelp_logo.png';
+import Logo_f from '../assets/f_logo.png';
+import Logo_i from '../assets/Insta_logo.png';
 
 
 function Footer() {
@@ -10,9 +10,9 @@ function Footer() {
     <div className="footer">
       <p>6116 Mack Road Sacramento, CA 95823</p>
       <div className="socialMedia">
-        <a target="_blank" href="https://www.instagram.com/phoru.usa/?hl=en"><img src={Logo_i} height={35} width={35} /></a> {"                           "} 
-        <a target="_blank" href="https://www.facebook.com/phorurestaurant/"><img src={Logo_f} height={35} width={35} /></a>  {"                           "}  
-        <a target="_blank" href="https://www.yelp.com/biz/pho-ru-vietnamese-restaurant-sacramento"><img src={Logo_y} height={35} width={80}/></a>
+        <a target="_blank" rel="noreferrer" href="https://www.instagram.com/phoru.usa/?hl=en"> <img src={Logo_i} alt="insta-log" height={35} width={35} /></a> {"                           "} 
+        <a target="_blank" rel="noreferrer" href="https://www.facebook.com/phorurestaurant/"><img src={Logo_f} alt="facebook-log"  height={35} width={35} /></a>  {"                           "}  
+        <a target="_blank" rel="noreferrer" href="https://www.yelp.com/biz/pho-ru-vietnamese-restaurant-sacramento"><img src={Logo_y} alt="yelp-log" height={35} width={80}/></a>
       </div>
       <p>&copy; 2022</p>
     </div>

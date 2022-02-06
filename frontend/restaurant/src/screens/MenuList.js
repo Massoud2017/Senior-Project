b7f5820f4cbd0c363fe0fa5b0edc88905133ea3d
misbatabassum <84794818+misbatabassum@users.noreply.github.com>
2022-02-05 20:28:58 -0800

    Added all menu items with name, category type, and price

--- frontend/restaurant/src/screens/MenuList.js
+++ frontend/restaurant/src/screens/MenuList.js
@@ -3,19 +3,254 @@ import GarlicChilliPrawn from '../assets/r1.PNG';
 import GrilledPorkChops from '../assets/r2.PNG';
 
 export const MenuList = [
-    { 
-        name: 'k1',
+    {   
+        type: 'Appetizers and Salads',
+        name: 'Ru`s Special Trio',
+        price: 9.95 
+    },
+    {   
+        type: 'Appetizers and Salads',
+        name: 'Herb Roasted Quail',
+        price: 8.25
+    },
+    {   
+        type: 'Appetizers and Salads',
+        name: 'Crispy Salt and Pepper Calamari',
+        price: 7.95
+    },
+    {   
+        type: 'Appetizers and Salads',
+        name: 'Homemade Style Crunchy Egg Rolls',
+        price: 5.65
+    },
+    {   
+        type: 'Appetizers and Salads',
+        name: 'Wok-Tossed Baby Clam Served w/ Shrimp Chip',
+        price: 9.95
+    },
+    {   
+        type: 'Appetizers and Salads',
+        name: 'Lemon Cured Shrimp Cabbage Salad',
+        price: 9.95
+    },
+    {   
+        type: 'Appetizers and Salads',
+        name: 'Lotus Root Salad Topped w/ Shrimp & Pork',
+        price: 8.75
+    },
+    {   
+        type: 'Appetizers and Salads',
+        name: 'Chicken Salad',
+        price: 7.75
+    },
+    {   
+        type: 'Pho - Beef Noodle Soup',
+        name: 'Pho Combo w/ Rare Steak, Brisket, Tendon & Tripe',
+        price: 10.75
+    },
+    {   
+        type: 'Pho - Beef Noodle Soup',
+        name: 'Rare Steak, Well-done Brisket and Tendon',
+        price: 10.75
+    },
+    {   
+        type: 'Pho - Beef Noodle Soup',
+        name: 'Rare Steak, Well-done Brisket and Tripe',
+        price: 10.75
+    },
+    {   
+        type: 'Pho - Beef Noodle Soup',
+        name: 'Rare Steak, Well-done Brisket and Meatballs',
+        price: 10.75
+    },
+    {   
+        type: 'Pho - Beef Noodle Soup',
+        name: 'Rare Steak and Well-Done Brisket',
+        price: 10.75
+    },
+    {   
+        type: 'Pho - Beef Noodle Soup',
+        name: 'Rare Steak and Beef Meatballs',
+        price: 10.75
+    },
+    {   
+        type: 'Pho - Beef Noodle Soup',
+        name: 'Rare Steak',
+        price: 10.75
+    },
+    {   
+        type: 'Pho - Beef Noodle Soup',
+        name: 'Pho with Chicken',
+        price: 9.75
+    },
+    {   
+        type: 'Pho - Beef Noodle Soup',
+        name: 'Pho with Shrimps',
+        price: 10.95
+    },
+    {   
+        type: 'Pho - Beef Noodle Soup',
+        name: 'Spicy Sate Pho',
+        price: 10.75
+    },
+    {   
+        type: 'Pho - Beef Noodle Soup',
+        name: 'Pho w/ Rare Filet Mignon (Quarter Pound Tenderloin)',
+        price: 11.95
+    },
+    {   
+        type: 'Noodle Soups',
+        name: 'Braised Duck Soup with Egg Noodle',
+        price: 11.75
+    },
+    {   
+        type: 'Noodle Soups',
+        name: 'Wonton Spicy Sate Soup w/ Egg or Rice Noodle',
+        price: 10.75
+    },
+    {   
+        type: 'Noodle Soups',
+        name: 'Thick Crab Soup q/ Tapioca Noodle or Udon',
+        price: 10.95
+    },
+    {   
+        type: 'Shareable Dishes',
+        name: 'BoBo Butter Crunchy Beef',
+        price: 11.95
+    },
+    {   
+        type: 'Shareable Dishes',
+        name: 'Lemon Grass Chicken',
+        price: 11.45
+    },
+    {   
+        type: 'Shareable Dishes',
+        name: 'Citrus Butter Prawn (Whole)',
+        price: 13.95
+    },
+    {   
+        type: 'Shareable Dishes',
+        name: 'Orange Glazed Crispy Shell Shrimp',
+        price: 13.95
+    },
+    {   
+        type: 'Shareable Dishes',
+        name: 'Spicy Satay Squid',
+        price: 11.95
+    },
+    {   
+        type: 'Shareable Dishes',
+        name: 'Crispy Garlic Rainbow Trout (Whole)',
+        price: 21.00
+    },
+    {   
+        type: 'Shareable Dishes',
+        name: 'Herb-Braised Duck (Half)',
+        price: 14.95
+    },
+    {   
+        type: 'Shareable Dishes',
+        name: 'Eggplant w/ Sweet & Sour Sauce',
+        price: 0.00
+    },
+    {   
+        type: 'Shareable Dishes',
+        name: 'Green Bean & Mushreem w/ Sate Sauce',
+        price: 0.00
+    },
+    {   
+        type: 'Shareable Dishes',
+        name: 'Green Bean & Mushroom w/ Sate Sauce',
+        price: 0.00
+    },
+    {
+        type: 'One Dish Meals',
+        name: 'Special Green Fried Rice w/ Cornish Game Hen',
         price: 13.95, 
         image: SpecialGreenFriedRice
     },
-    { 
-     name: 'r1',
-     price: 10.95,
-     image: GarlicChilliPrawn
+    {
+        type: 'One Dish Meals',
+        name: 'Green Fried Rice w/ Grilled Garlic Prawns',
+        price: 12.95, 
+        image: SpecialGreenFriedRice
     },
     { 
-        name: 'r2',
+        type: 'One Dish Meals',
+        name: 'Grilled Garlic Prawns & Pork Chop w/ Steamed Jasmine Rice',
+        price: 10.95,
+        image: GarlicChilliPrawn
+    },
+    { 
+        type: 'One Dish Meals',
+        name: 'Grilled Pork Chops w/ Steamed Jasmine Rice',
         price: 10.45, 
-        image: GrilledPorkChops}
+        image: GrilledPorkChops},
+    { 
+        type: 'One Dish Meals',
+        name: 'Grilled Garlic Prawns & Pork Chop w/ Steamed Jasmine Rice',
+        price: 10.95,
+        image: GarlicChilliPrawn
+    },
+    { 
+        type: 'Beverages',
+        name: 'Iced Coffee - Vietnamese Dark Roast Coffee w/ Condensed Milk',
+        price: 4.35,
+    },
+    { 
+        type: 'Beverages',
+        name: 'Soda Egg Milk',
+        price: 4.89,
+    },
+    { 
+        type: 'Beverages',
+        name: 'Sparkling Mango Juice',
+        price: 3.85,
+    },
+    { 
+        type: 'Beverages',
+        name: 'Thai Tea',
+        price: 4.25,
+    },
+    { 
+        type: 'Beverages',
+        name: 'Passion Fruit w/ Chia',
+        price: 3.85,
+    },
+    { 
+        type: 'Beverages',
+        name: 'Fresh Squeezed Orange Juice',
+        price: 4.25,
+    },
+    { 
+        type: 'Beverages',
+        name: 'Soft Drinks (Coke, Sprite, Sunkist, etc.)',
+        price: 1.85,
+    }, 
+    { 
+        type: 'Beverages',
+        name: 'Avacado Smoothie',
+        price: 4.65,
+    },
+    { 
+        type: 'Beverages',
+        name: 'Might Mango Smoothie',
+        price: 4.65,
+    },
+    { 
+        type: 'Beverages',
+        name: 'Strawberry Banana Smoothie',
+        price: 4.65,
+    },
+    { 
+        type: 'Beverages',
+        name: 'Matcha Green Tea Smoothie',
+        price: 4.65,
+    },
+    { 
+        type: 'House Special Dessert',
+        name: 'Pandan Caramel Flan',
+        price: 3.95,
+    }
 ];
 

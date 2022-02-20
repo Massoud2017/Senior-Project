0cb022330e54ed47e7b1207d713b5eee56cd07ec
misbatabassum <84794818+misbatabassum@users.noreply.github.com>
2022-02-20 12:22:56 -0800

    #78

--- frontend/restaurant/src/screens/MenuItems.js
+++ frontend/restaurant/src/screens/MenuItems.js
@@ -9,6 +9,7 @@ function MenuItems({menuItem, onAddToCart}) {
                         <div className="item-container">
                             <img src={item.image} alt=""/>
                             <h2>{item.name}</h2>
+                            <h1>{item.vietnamese_name}</h1>
                             <p>${item.price}</p>
                             {/* <button onClick={onAddToCart} className="add-cart-btn">Add To Cart</button> */}
                             <button onClick={() => onAddToCart(item)} className="add-cart-btn">Add To Cart</button>

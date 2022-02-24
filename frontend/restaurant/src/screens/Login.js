fc21cb9a79d47637956a98f7c6047ea7b213987f
Duc Nguyen <ducnguyen@csus.edu>
2022-02-24 12:30:08 -0800

    #81 Check and update the sign in page UI

--- frontend/restaurant/src/screens/Login.js
+++ frontend/restaurant/src/screens/Login.js
@@ -1,5 +1,6 @@
 import React , {useState, useEffect} from 'react';
 import Axios from 'axios'
+// import GoogleLoginButton from './components/GoogleLoginButton'
 
 
 function Login() {
@@ -10,25 +11,38 @@ function Login() {
      Axios.post('http://localhost:3001/api/insert', {userID:userID, password:password}).then(() =>{
        alert("successful insert");
      });
-  }; 
-    return (
-      <div>
-        
-         <form className="form-login">
-          
-           <label for ="id"> Username </label>  
-                <input class="userId" type="userid" id="Username" name="Username" placeholder="Username"  onChange={(e)=> {
-                  setUserID(e.target.value)
-                }}/><br/> <br/>
-            
-            <label for ="password" > Password </label>  
-            <input type="password" id="password" name="password" placeholder="Password" onChange={(e)=> {
-                  setPassword(e.target.value)
-                }}/><br/> <br/>
-                <button class="btn-login" onClick={SignIn}>Sign In</button>
-          </form> 
+  };
+  
+  return (
+    <div className='login-body'>
+      <div className='login-container'>
+        <h1>Login</h1>
+        {/* <form method='post' className="form-login"> */}
+        <form>
+          <div className='form-text-field'>
+            <input type='text' required id='username' name='username' 
+              placeholder='Username'  onChange={(e)=> {
+              setUserID(e.target.value)
+            }}/>
+          </div>
+          <div className='form-text-field'>
+            <input type='password' required id='password' name='password' 
+              placeholder='Password' onChange={(e)=> {
+                setPassword(e.target.value)
+            }} />
+          </div>
+          {/* <button className='form-login-btn' onClick={SignIn}>Sign In</button> */}
+          <input type='submit' value='Login' />
+          {/*-- For Nam to implement Google login */}
+          {/* <GoogleLoginButton className='google-login-btn' /> */}
+          {/* -- End of Google Login */}
+          <div className='signup-link'>
+            Not a member? <a href='#'>Signup</a>
+          </div>
+        </form>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Login;

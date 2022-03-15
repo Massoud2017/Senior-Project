bd68b53ee4064d8a403718d3ef373afc53c5a34b
Nam Tran <61358799+trankhainam97@users.noreply.github.com>
2022-03-14 22:32:19 -0700

    #101 Add Google login and reset password option

--- frontend/restaurant/src/screens/Home.js
+++ frontend/restaurant/src/screens/Home.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './components/home-style.css';
-import './components/home-script.js';
 import front from './components/home-front.jpg';
 import interior from './components/home-interior.jpg';
 import food from './components/home-food.jpg';
 
 function Home() {
+    document.addEventListener("DOMContentLoaded", function (event) {
+        const images = document.querySelectorAll(".intro-slideshow img");
+        const nextImageDelay = 5000;
+        let currentImageCounter = 0;
+      
+        images[currentImageCounter].style.opacity = 1;
+        setInterval(nextImage, nextImageDelay);
+      
+        function nextImage() {
+          images[currentImageCounter].style.opacity = 0;
+          currentImageCounter = (currentImageCounter + 1) % images.length;
+          images[currentImageCounter].style.opacity = 1;
+        }
+    });
 
     return (
         <div className="intro">

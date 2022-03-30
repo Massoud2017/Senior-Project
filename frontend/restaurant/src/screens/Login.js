83e6612039d638c6907e67adce9c1723682cf05b
misbatabassum <84794818+misbatabassum@users.noreply.github.com>
2022-03-30 16:26:02 -0700

    #126

--- frontend/restaurant/src/screens/Login.js
+++ frontend/restaurant/src/screens/Login.js
@@ -24,39 +24,43 @@ function Login() {
   
   return (
     <div className='login-body'>
-      <div className='login-container'>
-        <h1>Login</h1>
-        <div className='login-form'>
-          <div className='form-text-field'>
-            <input
-							required
-							type='text'
-              placeholder='Email'
-							onChange={(event)=> {
-              	setUsername(event.target.value);
-            	}}
-						/>
-          </div>
-          <div className='form-text-field'>
-            <input
-							required
-							type='password'
-              placeholder='Password'
-							onChange={(e)=> {
-                setPassword(e.target.value);
-            	}}
-						/>
-          </div>
-            <button className='form-login-btn' onClick={login}>Login</button>
-          <div className='g-signin'>
-            <GoogleLoginButton/>
-          </div>
-          <div className='signup-link'>
-						Not a member? <Link to='/signup'>Register an account</Link><br/>
-            Forgot your password? <Link to='/changepassword'>Reset password</Link>
+        <div className='login-top-text-field'>
+          <h2>SIGN IN</h2>
+          <h3>VIEW YOUR ORDER HISTORY</h3>
+            <div className='login-container'>
+              <h1>Login</h1>
+              <div className='login-form'>
+                <div className='form-text-field'>
+                  <input
+                    required
+                    type='text'
+                    placeholder='Email'
+                    onChange={(event)=> {
+                      setUsername(event.target.value);
+                    }}
+                  />
+                </div>
+                <div className='form-text-field'>
+                  <input
+                    required
+                    type='password'
+                    placeholder='Password'
+                    onChange={(e)=> {
+                      setPassword(e.target.value);
+                    }}
+                  />
+                </div>
+                  <button className='form-login-btn' onClick={login}>Login</button>
+                <div className='g-signin'>
+                  <GoogleLoginButton/>
+                </div>
+                <div className='signup-link'>
+                  Not a member? <Link to='/signup'>Create an account</Link><br/>
+                  Forgot your password? <Link to='/changepassword'>Reset password</Link>
+                </div>
+              </div>
           </div>
         </div>
-      </div>
     </div>
   )
 }

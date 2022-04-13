158fde7f529620add928a7ab9634626101e9d1e8
misbatabassum <84794818+misbatabassum@users.noreply.github.com>
2022-04-12 17:32:43 -0700

    NPRRW-139: UI Mod. for Forgot/Reset Password

--- frontend/restaurant/src/screens/Profile.js
+++ frontend/restaurant/src/screens/Profile.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import {useNavigate } from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
